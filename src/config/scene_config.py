ccfc0e52fbaca35b9e2a94f7ecab15653f895ead
abenezer147 <abenezerdaniel147@gmail.com>
2026-05-12 05:57:01 +0300

    Fully tested for multiple elements at the same time.

--- src/config/scene_config.py
+++ src/config/scene_config.py
@@ -1,4 +1,4 @@
 scene_config = {
-    "resolution": (10, 10),
+    "resolution": (12, 12),
     "background_color": (255, 100, 0)
 }
--- src/main.py
+++ src/main.py
@@ -2,13 +2,40 @@ from scene import create_scene
 from config import scene_config
 from elements import create_rectangle
 
-rectangle_properties = {
-    "size": [20, 20],
-    "position": [7, 0],
+rectangle_one_properties = {
+    "size": [3, 3],
+    "position": [0, 0],
     "fill": [255, 255, 255]
 }
 
-rectangle = create_rectangle(rectangle_properties)
-elements = [rectangle]
+rectangle_two_properties = {
+    "size": [3, 3],
+    "position": [3, 3],
+    "fill": [0, 0, 0]
+}
+
+rectangle_three_properties = {
+    "size": [3, 3],
+    "position": [6, 6],
+    "fill": [255, 255, 255]
+}
+
+rectangle_four_properties = {
+    "size": [3, 3],
+    "position": [9, 9],
+    "fill": [0, 0, 0]
+}
+
+rectangle_one = create_rectangle(rectangle_one_properties)
+rectangle_two = create_rectangle(rectangle_two_properties)
+rectangle_three = create_rectangle(rectangle_three_properties)
+rectangle_four = create_rectangle(rectangle_four_properties)
+
+elements = [
+    rectangle_one,
+    rectangle_two,
+    rectangle_three,
+    rectangle_four
+]
 
 create_scene(scene_config, elements)
